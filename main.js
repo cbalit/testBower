9d5b80f01aca7c94f9448b9eb449b4fab2cbb0b3
cbalit <dou4872>
2014-06-18 17:22:12 +0200

    add init method

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
  * Created by adzs637 on 26/05/2014.
  */
 var testBower  ={
+    init:function(){
+        console.log('Init');
+    },
     hello:function(name){
         console.log('Hello '+ name);
     }
